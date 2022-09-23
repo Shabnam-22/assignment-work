0c333c1a90da798a2e8ff9e0b8785d34df8a2867
Shabnam-22 <114242792+Shabnam-22@users.noreply.github.com>
2022-09-23 20:37:28 +0530

    Add files via upload

---
+++ SwapNum.java
@@ -0,0 +1,23 @@
+package assignments;
+
+public class SwapNum {
+
+
+		public static void main(String[] args) {
+
+			int a=10;
+			int b=20;
+			int c;
+			
+			//swapping numbers
+			
+			c=a;
+			a=b;
+			b=c;
+			
+			System.out.println("a equals to " +a +" "+"after swapping");
+			System.out.println("b equals to " +b +" " +"after swapping");
+		}
+	
+
+}
